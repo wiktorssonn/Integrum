dcb222baa7e12fc0a8eb6c464cecfd0f09dc0a4c
wiktorssonn <56696783+wiktorssonn@users.noreply.github.com>
2020-04-03 16:22:31 +0200

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -3,43 +3,16 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm, ValidationError
 #Kryperar användarnas lösenord
 from flask_bcrypt import Bcrypt
+#Login funktion
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+#Importerar psycopg2 för att skriva till databasen
 import psycopg2
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "fbc07874e91feeaa1b0e8dcb400930bf"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-#db= SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
-'''
-#Connecting to database
-try:
-    connection = psycopg2.connect(user = "aj8772",
-                              password = "z7zz9fgh",
-                              host = "pgserver.mah.se",
-                              database = "integrum_db")
-    cursor = connection.cursor()
-    print("Connected to database")
-except (Exception, psycopg2.Error) as error:
-    print("Error while fetching data from PostgreSQL", error)
-'''
-
-#cursor.execute("create table users (user_id serial primary key, username varchar(20) unique not null, email varchar(120) not null, password varchar(60) not null)")
-#connection.commit()
-#route_records = cursor.fetchall()
-
-'''class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self): #Hur det printas ut?
-        return User("{}, {}, {}, {}".format(self.username, self.email, self.image_file))
-'''
-
-
+login_manager = LoginManager(app)
 
 
 @app.route("/")
@@ -60,6 +33,13 @@ def om_oss():
 @app.route("/kontakt")
 def kontakt():
     return render_template("kontakt.html", title="Kontakt")
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -94,11 +74,50 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "blabla@gmail.com" and form.password.data == "password":
-            flash("Inloggning lyckades!", "success")
-            return redirect(url_for("hem"))
+        '''
+        #Ansluter till databasen
+        try:
+            connection = psycopg2.connect(user = "aj8772",
+                                    password = "z7zz9fgh",
+                                    host = "pgserver.mah.se",
+                                    database = "integrum_db")
+            cursor = connection.cursor()
+            print("Connected to database")
+        except (Exception, psycopg2.Error) as error:
+            print("Error while fetching data from PostgreSQL", error)
+            
+        #Emailen som angetts i formuläret läggs i variabeln email
+        email = form.email.data
+        password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        print("Email: {}".format(email))
+        print("Lösenord: {}".format(password))
+
+
+        #Hämtar alla email från users table i databasen
+        cursor.execute("select * from users")
+        all_users = cursor.fetchall()
+        
+        user_exist = False
+        for user in all_users:
+            if email.lower() == user[2].lower and password == user[3]:
+                print(user[2],user[3])
+                user_exist = True
+            else:
+                print(user[2],user[3])
+                print("hej")
+        #Om emailen redan finns, skriv ut felmeddelande
+        #if email_in_use == True:
+            #raise ValidationError("Denna mail finns redan registrerad!")
+        
+        
+        
+        user = User.query.filter_by(username=username.lower()).first()
+        if user:
+            if login_user(user):
+                flash("Inloggning lyckades!", "success")
+                return redirect(url_for("hem"))
         else:
-            flash("Inloggning misslyckades, Kontrollera användarnamn och lösenord", "danger")
+            flash("Inloggning misslyckades, Kontrollera användarnamn och lösenord", "danger")'''
     return render_template("login.html", title="Logga in", form=form)
 
 
