65355e212dbbe1ce8edc51aa54a43811f9cb6f96
jgoransson <62560223+jgoransson@users.noreply.github.com>
2020-05-18 11:52:26 +0200

    delete buy/sell

--- flask_integrum/main/forms.py
+++ flask_integrum/main/forms.py
@@ -13,9 +13,3 @@ class PostAssignment(FlaskForm):
     submit = SubmitField('Lägg till')
 
 
-class PostItem(FlaskForm):
-    Title = StringField('Titel', validators=[DataRequired(message="Du måste ange en titel!")])
-    course = TextAreaField('Kurs:', validators=[DataRequired(message="Du måste ange vilken kurs boken används i!")])
-    Contact = StringField('Kontakt: ', validators=[DataRequired(message="Du måste ange email eller telefonnummer!")])
-    price = StringField('Pris: ', validators=[DataRequired(message="Du måste ange ett pris för boken!")])
-    submit = SubmitField('Lägg till')
