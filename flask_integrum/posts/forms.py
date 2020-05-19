01c53634ba3487aaf2e97cdac3b431e36710cdb2
jgoransson <62560223+jgoransson@users.noreply.github.com>
2020-05-19 11:13:43 +0200

    docgransk

--- flask_integrum/posts/forms.py
+++ flask_integrum/posts/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError
-
+from wtforms.validators import DataRequired, ValidationErro
 from flask import flash
 from flask_login import current_user
 
 
-
+#Klass för formuläret till forumet
 class PostForm(FlaskForm):
-    title = StringField('Titel', validators=[DataRequired(message="Du måste ange en titel!")])
-    content = TextAreaField('Innehåll', validators=[DataRequired(message="Du måste skriva något!")])
+    title = StringField('Titel', 
+                        validators=[DataRequired(
+                        message="Du måste ange en titel!")])
+
+    content = TextAreaField('Innehåll',
+                            validators=[DataRequired(
+                            message="Du måste skriva något!")])
+
     submit = SubmitField('Skicka')
-    
